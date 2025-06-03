b0793890efe419439edb4c212dbc0c1375a754b7
nfgrep <nathan.fabergood@shopify.com>
2025-06-03 17:44:50 -0400

    Small optimization for finding .roast

--- lib/roast/config.rb
+++ lib/roast/config.rb
@@ -7,15 +7,14 @@ module Roast
 
     class << self
       def root(starting_path = Dir.pwd, ending_path = File.dirname(Dir.home))
-        paths = []
         candidate = starting_path
         while candidate != ending_path
-          paths << File.join(candidate, ".roast")
+          break if Dir.exist?(File.join(candidate, ".roast"))
+
           candidate = File.dirname(candidate)
         end
 
-        first_existing = paths.find { |path| Dir.exist?(path) }
-        first_existing || paths.first
+        File.join(candidate, ".roast")
       end
     end
   end
