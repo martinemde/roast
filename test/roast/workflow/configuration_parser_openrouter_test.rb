f72e849868c8fbbc98c59daf9c657e298c8f4108
Obie Fernandez <obiefernandez@gmail.com>
2025-06-02 20:18:32 -0600

    fix flaky test

---
+++ test/roast/workflow/configuration_parser_openrouter_test.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "test_helper"
+require "open_router"
+
+module Roast
+  module Workflow
+    class ConfigurationParserOpenRouterTest < ActiveSupport::TestCase
+      def setup
+        @workflow_path = File.expand_path("../../fixtures/files/openrouter_workflow.yml", __dir__)
+      end
+
+      def test_configure_openrouter_client
+        mock_openrouter_client = mock("OpenRouter::Client")
+        OpenRouter::Client.stubs(:new).with({ access_token: "test_openrouter_token" }).returns(mock_openrouter_client)
+
+        assert_nothing_raised { ConfigurationParser.new(@workflow_path) }
+      end
+    end
+  end
+end
